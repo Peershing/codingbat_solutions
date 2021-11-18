f005916aade57430a2f15f9d45e051fffce3af49
Jakub <james.lukaszewski@gmail.com>
2021-11-18 15:27:59 +0100

    Warmup-2 added

---
+++ src/warmup_2/DoubleX.java
@@ -0,0 +1,21 @@
+package warmup_2;
+
+/*
+	Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
+*/
+
+public class DoubleX {
+	boolean doubleX(String str) {
+		int xCount = 0;
+		for (int i = 0; i <= str.length()-2; i++) {
+			if (str.charAt(i+1) == 'x' && xCount == 0 && str.charAt(i) == 'x') {
+				return true;
+			}
+			if (str.charAt(i) == 'x') {
+				xCount++;
+			}
+		}
+		  
+		return false;
+	}
+}
---
+++ src/warmup_2/SpringSplosion.java
@@ -0,0 +1,17 @@
+package warmup_2;
+
+/*
+	Given a non-empty string like "Code" return a string like "CCoCodCode".
+*/
+
+public class SpringSplosion {
+	public String stringSplosion(String str) {
+		StringBuilder result = new StringBuilder("");
+		  
+		for (int i = 0; i < str.length(); i++) {
+		  result = result.append(str.substring(0, i+1));
+		}
+		  
+		return result.toString();
+	}
+}
