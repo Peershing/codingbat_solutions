cb4ae85ded13ebed8064d73362a36404ee862234
Jakub <james.lukaszewski@gmail.com>
2021-11-18 18:03:08 +0100

    Logic-2 added

---
+++ src/logic_2/Blackjack.java
@@ -0,0 +1,23 @@
+package logic_2;
+
+/*
+	Given 2 int values greater than 0, return whichever value is nearest to 21 without going over.
+	Return 0 if they both go over.
+*/
+
+public class Blackjack {
+	public int blackjack(int a, int b) {
+		int limit = 21;
+		  
+		if (a > limit && b > limit) {
+		    return 0;
+		}
+		else if (a > limit) {
+		    return b;
+		}
+		else if (b > limit) {
+		    return a;
+		}
+		return (a > b)? a : b;
+	}
+}
---
+++ src/logic_2/CloseFar.java
@@ -0,0 +1,17 @@
+package logic_2;
+
+/*
+	Given three ints, a b c, return true if one of b or c is "close" (differing from a by at most 1), while the other is "far", differing from both other values by 2 or more. Note: Math.abs(num) computes the absolute value of a number.
+*/
+
+public class CloseFar {
+	public boolean closeFar(int a, int b, int c) {
+		int dist1 = (int) Math.abs(b-a);
+		int dist2 = (int) Math.abs(c-a);
+		int dist3 = (int) Math.abs(c-b);
+		  
+		boolean check = ((dist1 <= 1 || dist2 <= 1) && (dist1 >= 2 && dist3 >= 2 || dist2 >= 2 && dist3 >= 2));
+
+		return check? true : false;
+	}
+}
