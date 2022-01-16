d4cb0cc82248447da8755fc6f6868342938cafc7
James <james.lukaszewski@gmail.com>
2022-01-16 22:54:40 +0100

    AP-1 added part 2

---
+++ src/ap/one/WordsFront.java
@@ -0,0 +1,18 @@
+package api.one;
+
+/*
+    Given an array of strings, return a new array containing the first N strings. N will be in the range
+    1..length.
+*/
+
+public class ScoresAverage {
+
+    public String[] wordsFront(String[] words, int n) {
+        String[] arr = new String[n];
+
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = words[i];
+
+        return arr;
+    }
+}
