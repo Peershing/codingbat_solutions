ba3e51934df85dcc1e2b188694a760819edad5af
James <james.lukaszewski@gmail.com>
2022-01-17 22:40:45 +0100

    AP-1 added part 3

---
+++ src/ap/one/WordsWithoutList.java
@@ -0,0 +1,19 @@
+package api.one;
+
+/*
+    Given an array of strings, return a new List (e.g. an ArrayList) where all the strings of the given
+    length are omitted. See wordsWithout() below which is more difficult because it uses arrays.
+*/
+
+public class WordsWithoutList {
+
+    public List wordsWithoutList(String[] words, int len) {
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() != len)
+                list.add(words[i]);
+        }
+        return list;
+    }
+}
