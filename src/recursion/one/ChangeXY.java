7d271a1c2424f7dbc57ec5136fa5150a0ba72ad3
James <james.lukaszewski@gmail.com>
2022-01-22 13:05:20 +0100

    Recursion-1 added part-11

---
+++ src/recursion/one/ChangeXY.java
@@ -0,0 +1,13 @@
+package recursion.one;
+
+/*
+    Given a string, compute recursively (no loops) a new string where all the lowercase 'x' chars have been changed
+    to 'y' chars.
+*/
+
+public class ChangeXY {
+
+    public String changeXY(String str) {
+        return str.replace('x', 'y');
+    }
+}
