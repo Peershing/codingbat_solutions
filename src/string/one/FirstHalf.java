0cab2289724dad429c391359b5334c01baa09d52
James <james.lukaszewski@gmail.com>
2022-01-31 23:52:49 +0100

    String-1 added 4.0

---
+++ src/string/one/FirstHalf.java
@@ -0,0 +1,14 @@
+package string.one;
+
+/*
+    Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+*/
+
+public class FirstHalf {
+
+    public String firstHalf(String str) {
+        if (str.length() < 2) {
+            return "";
+        }
+    }
+}
