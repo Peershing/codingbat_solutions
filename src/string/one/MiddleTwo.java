be4c89cd25d8b0783464fbe888321ed93c18013f
James <james.lukaszewski@gmail.com>
2022-02-07 13:59:42 +0100

    String-1 added 7.0

---
+++ src/string/one/MiddleTwo.java
@@ -0,0 +1,13 @@
+package string.one;
+
+/*
+    Given a string of even length, return a string made of the middle two chars, so the string "string" yields "ri".
+    The string length will be at least 2.
+*/
+
+public class MiddleTwo {
+
+    public String middleTwo(String str) {
+        return str.substring(str.length()/2-1, str.length()/2+1);
+    }
+}
