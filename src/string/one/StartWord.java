b7ddf2a6fe2ca9690311fd9c5e985a917cf663bc
James <james.lukaszewski@gmail.com>
2022-04-04 21:38:01 +0200

    String-1 added 11.0

---
+++ src/string/one/StartWord.java
@@ -0,0 +1,23 @@
+package string.one;
+
+/*
+    Given a string and a second "word" string, we'll say that the word matches the string if it appears at the front
+    of the string, except its first char does not need to match exactly. On a match, return the front of the string,
+    or otherwise return the empty string. So, so with the string "hippo" the word "hi" returns "hi" and "xip" returns
+    "hip". The word will be at least length 1.
+*/
+
+public class StartWord {
+
+    public String startWord(String str, String word) {
+        if (str.length() < word.length()) {
+            return "";
+        }
+
+        if (str.substring(1, word.length()).equals(word.substring(1, word.length()))) {
+            return str.substring(0, word.length());
+        }
+
+        return "";
+    }
+}
